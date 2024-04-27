b95e4796cec093d1b31bcc4fea261086a9d0a521
zerocyberhacker <zerocyberhacker@gmail.com>
2024-04-27 11:12:45 +0800

    update

--- index.ts
+++ index.ts
@@ -1,20 +1,14 @@
+/** @format */
+
 import * as crypto from "crypto";
 
-const plaintext: string = "route 53";
+const plaintext: string = `admin`;
 const hashAlgorithm: string = "sha256";
 const hashEncoding: BufferEncoding = "hex"; // You can choose other encodings such as base64 or utf8
-const hash: string = crypto.createHash(hashAlgorithm).update(plaintext).digest(hashEncoding);
+const hash: string = crypto
+  .createHash(hashAlgorithm)
+  .update(plaintext)
+  .digest(hashEncoding);
 
-console.log("WANS -> Wallet Address Name Service")
-console.log("The message             : "+plaintext);
-console.log("The sha256 hash result  : "+hash);
-
-const targetHash = 'b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9';
-
-if (hash === targetHash) {
-    console.log('Match found!');
-} else {
-    console.log('No match found.');
-}
-
-
+console.log("The message             : " + plaintext);
+console.log("The private key result  : " + hash);
